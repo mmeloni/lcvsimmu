9ebeb708445841d6f97814efab953db125fb74c4
Michele Meloni <cleaversdev@gmail.com>
2020-10-14 18:55:31 +0200

    add lc batch insertion

--- benchmark.go
+++ benchmark.go
@@ -14,10 +14,11 @@ import (
 
 func main() {
 	benchmarkWork := 1000
-	log.Printf("Ledger compliance vs Immudb SDK. Each loop are %d safeset", benchmarkWork)
+	sdkApiKey := "ugplxlpxgenyisojbdzbjzcgickpzmhupkey"
+	log.Printf("Ledger compliance vs Immudb SDK. Each loop are %d set", benchmarkWork)
 	for loop:= 0; loop < 5; loop ++ {
 
-		client := sdk.NewLcClient(sdk.ApiKey("eeicdeqliaxfpiezgtiqljfhdhvbjnrgyvml"), sdk.Host("localhost"), sdk.Port(3324))
+		client := sdk.NewLcClient(sdk.ApiKey(sdkApiKey), sdk.Host("localhost"), sdk.Port(3324))
 		err := client.Connect()
 		if err != nil {
 			log.Fatal(err)
@@ -25,7 +26,7 @@ func main() {
 		start := time.Now()
 
 		for i:=0; i<=benchmarkWork;i++{
-			_, err := client.SafeSet(context.Background(), []byte(fmt.Sprintf("%d", i)), []byte(fmt.Sprintf("%d", i)))
+			_, err := client.Set(context.Background(), []byte(fmt.Sprintf("%d", i)), []byte(fmt.Sprintf("%d", i)))
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -51,13 +52,36 @@ func main() {
 		md = metadata.Pairs("authorization", "Bearer "+respUse.Token)
 		ctx = metadata.NewOutgoingContext(context.Background(), md)
 		for i:=0; i<=benchmarkWork;i++{
-			_, err := immuclient.SafeSet(ctx, []byte(fmt.Sprintf("%d", i)), []byte(fmt.Sprintf("%d", i)))
+			_, err := immuclient.Set(ctx, []byte(fmt.Sprintf("%d", i)), []byte(fmt.Sprintf("%d", i)))
 			if err != nil {
 				log.Fatal(err)
 			}
 		}
 		elapsedImmu := time.Since(startImmu)
 		log.Printf("Elapsed %s on Immudb \tloop %d", elapsedImmu, loop)
+
+
+		startBatchLc := time.Now()
+		/*client1 := sdk.NewLcClient(sdk.ApiKey(sdkApiKey), sdk.Host("localhost"), sdk.Port(3324))
+		err = client1.Connect()*/
+		if err != nil {
+			log.Fatal(err)
+		}
+		start = time.Now()
+		skv := &schema.KVList{		}
+		for i:=0; i<=benchmarkWork;i++{
+			kv := &schema.KeyValue{
+				Key:   []byte(fmt.Sprintf("key%d",i)),
+				Value: []byte(fmt.Sprintf("val%d",i)),
+			}
+			skv.KVs = append(skv.KVs, kv)
+		}
+		_, err = client.SetBatch(ctx, skv)
+		if err != nil {
+			log.Fatal(err)
+		}
+		elapsedBatchLc := time.Since(startBatchLc)
+		log.Printf("Elapsed %s on batch lc \tloop %d", elapsedBatchLc, loop)
 	}
 
 }
--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.15
 
 require (
 	github.com/codenotary/immudb v0.8.0
-	github.com/vchain-us/ledger-compliance-go v0.0.0-20201012160855-de9cc58c77da
+	github.com/vchain-us/ledger-compliance-go v0.0.0-20201014151406-5654aa5886dc
 	google.golang.org/grpc v1.29.1
 )
 
-replace github.com/codenotary/immudb v0.8.0 => github.com/codenotary/immudb v0.0.0-20201009135111-4ecdf1bf01bd
+replace github.com/codenotary/immudb v0.8.0 => github.com/codenotary/immudb v0.0.0-20201014151548-74e7b6c86339
