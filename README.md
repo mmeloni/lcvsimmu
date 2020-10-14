63753c64444a6a46b4e84ddc2cb4e66ed798ee49
Michele Meloni <cleaversdev@gmail.com>
2020-10-14 23:45:25 +0200

    add args

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # lcvsimmu
+
+command launch
+
+benchmark {number of insertions} {apikey} {lc address} {lc port} {immudb address} {immudb port} 
+```bash
+./benchmark 1000 athlndeyjnyfqcnvoexjwlpjtltgcxtnqjyu localhost 3324 localhost 3322
+```
+to skip immudb benchmark
+
+```bash
+./benchmark 1000 athlndeyjnyfqcnvoexjwlpjtltgcxtnqjyu localhost 3324 
+```
--- benchmark.go
+++ benchmark.go
@@ -7,18 +7,44 @@ import (
 	immusdk "github.com/codenotary/immudb/pkg/client"
 	sdk "github.com/vchain-us/ledger-compliance-go/grpcclient"
 	"google.golang.org/grpc/metadata"
+	"os"
+	"strconv"
 
 	"log"
 	"time"
 )
 
 func main() {
-	benchmarkWork := 1000
-	sdkApiKey := "ugplxlpxgenyisojbdzbjzcgickpzmhupkey"
+	benchmarkWork, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	apikey := os.Args[2]
+	lcHost := os.Args[3]
+	lcPort, err := strconv.Atoi(os.Args[4])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(3)
+	}
+	var immuHost string
+	if len(os.Args) > 5{
+		immuHost = os.Args[5]
+	}
+	var immuPort int
+	if len(os.Args) > 6{
+		immuPort, err = strconv.Atoi(os.Args[6])
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
+	}
+
+
 	log.Printf("Ledger compliance vs Immudb SDK. Each loop are %d set", benchmarkWork)
 	for loop:= 0; loop < 5; loop ++ {
 
-		client := sdk.NewLcClient(sdk.ApiKey(sdkApiKey), sdk.Host("localhost"), sdk.Port(3324))
+		client := sdk.NewLcClient(sdk.ApiKey(apikey), sdk.Host(lcHost), sdk.Port(lcPort))
 		err := client.Connect()
 		if err != nil {
 			log.Fatal(err)
@@ -35,30 +61,33 @@ func main() {
 		log.Printf("Elapsed %s on lc \t\tloop %d", elapsed, loop)
 
 		//log.Printf("Immudb starting on loop %d", loop)
-		startImmu := time.Now()
-		immuclient, err := immusdk.NewImmuClient(immusdk.DefaultOptions())
-		if err != nil {
-			log.Fatal(err)
-		}
-		resp, err := immuclient.Login(context.Background(), []byte(`immudb`), []byte(`immudb`))
-		if err != nil {
-			log.Fatal(err)
-		}
-		md := metadata.Pairs("authorization", "Bearer "+resp.Token)
-		ctx := metadata.NewOutgoingContext(context.Background(), md)
-		respUse, err := immuclient.UseDatabase(ctx, &schema.Database{
-			Databasename: "defaultdb",
-		})
-		md = metadata.Pairs("authorization", "Bearer "+respUse.Token)
-		ctx = metadata.NewOutgoingContext(context.Background(), md)
-		for i:=0; i<=benchmarkWork;i++{
-			_, err := immuclient.Set(ctx, []byte(fmt.Sprintf("%d", i)), []byte(fmt.Sprintf("%d", i)))
+		if immuHost != "" || immuPort != 0 {
+			startImmu := time.Now()
+			immuclient, err := immusdk.NewImmuClient(immusdk.DefaultOptions().WithAddress(immuHost).WithPort(immuPort))
 			if err != nil {
 				log.Fatal(err)
 			}
+			resp, err := immuclient.Login(context.Background(), []byte(`immudb`), []byte(`immudb`))
+			if err != nil {
+				log.Fatal(err)
+			}
+			md := metadata.Pairs("authorization", "Bearer "+resp.Token)
+			ctx := metadata.NewOutgoingContext(context.Background(), md)
+			respUse, err := immuclient.UseDatabase(ctx, &schema.Database{
+				Databasename: "defaultdb",
+			})
+			md = metadata.Pairs("authorization", "Bearer "+respUse.Token)
+			ctx = metadata.NewOutgoingContext(context.Background(), md)
+			for i:=0; i<=benchmarkWork;i++{
+				_, err := immuclient.Set(ctx, []byte(fmt.Sprintf("%d", i)), []byte(fmt.Sprintf("%d", i)))
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+			elapsedImmu := time.Since(startImmu)
+			log.Printf("Elapsed %s on Immudb \tloop %d", elapsedImmu, loop)
 		}
-		elapsedImmu := time.Since(startImmu)
-		log.Printf("Elapsed %s on Immudb \tloop %d", elapsedImmu, loop)
+
 
 
 		startBatchLc := time.Now()
@@ -76,7 +105,7 @@ func main() {
 			}
 			skv.KVs = append(skv.KVs, kv)
 		}
-		_, err = client.SetBatch(ctx, skv)
+		_, err = client.SetBatch(context.Background(), skv)
 		if err != nil {
 			log.Fatal(err)
 		}
